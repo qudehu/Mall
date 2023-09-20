609fbe96ba1f42369a9f5cbc12c827b6f3daa182
qudehu <qudehu@163.com>
2023-09-20 09:50:10 +0800

    修改契约机制，将使用feign原生注解

--- order-openfeign/src/main/java/com/dehu/order/feign/ProductFeignService.java
+++ order-openfeign/src/main/java/com/dehu/order/feign/ProductFeignService.java
@@ -1,5 +1,7 @@
 package com.dehu.order.feign;
 
+import feign.Param;
+import feign.RequestLine;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @FeignClient(name = "product-nacos", path = "/product")
 public interface ProductFeignService {
 
-    @RequestMapping("/{id}")
-    String get(@PathVariable("id") Integer id);
+    @RequestLine("GET /{id}")
+    String get(@Param("id") Integer id);
 
 }
