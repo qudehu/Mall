08ab925c96bcbed19d4cae64f80efc1cda8331e7
qudehu <qudehu@163.com>
2023-09-20 10:05:52 +0800

    自定义拦截器添加所需参数，或做其他处理

--- product-nacos/src/main/java/com/dehu/order/controller/ProductController.java
+++ product-nacos/src/main/java/com/dehu/order/controller/ProductController.java
@@ -1,11 +1,15 @@
 package com.dehu.order.controller;
 
+import org.apache.http.HttpResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @Classname OrderController
