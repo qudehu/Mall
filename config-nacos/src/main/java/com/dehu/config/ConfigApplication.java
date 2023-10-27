a265ef48df7ce0a689fc73c15250fd1a7619b5d3
qudehu <qudehu@163.com>
2023-10-27 11:25:14 +0800

    nacos-config:实时获取配置文件中的更新

--- config-nacos/src/main/java/com/dehu/config/ConfigApplication.java
+++ config-nacos/src/main/java/com/dehu/config/ConfigApplication.java
@@ -2,11 +2,24 @@ package com.dehu.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class ConfigApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ConfigApplication.class, args);
+    public static void main(String[] args) throws InterruptedException {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(ConfigApplication.class, args);
+
+        // 持续监听并获取配置文件中的信息
+        while (true) {
+            String userName = applicationContext.getEnvironment().getProperty("user.name");
+            String userAge = applicationContext.getEnvironment().getProperty("user.age");
+            String config = applicationContext.getEnvironment().getProperty("user.config");
+            System.err.println("user name :" + userName + "; age: " + userAge + ";config:" + config);
+            TimeUnit.SECONDS.sleep(1);
+        }
+
     }
 }
