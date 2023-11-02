a35b1c833d41b021d22c27ea511c716e8b9dfb5c
qudehu <qudehu@163.com>
2023-11-02 10:01:43 +0800

    sentinel:单独使用sentinel，建立工程文件

---
+++ sentinel_demo/src/main/java/com/dehu/sentinel/SentinelApplication.java
@@ -0,0 +1,13 @@
+package com.dehu.sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelApplication.class, args);
+    }
+
+}
---
+++ sentinel_demo/src/main/java/com/dehu/sentinel/pojo/User.java
@@ -0,0 +1,5 @@
+package com.dehu.sentinel.pojo;
+
+@Data
+public class User {
+}
