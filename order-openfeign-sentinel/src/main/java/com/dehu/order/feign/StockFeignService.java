84bcc37c030935183ac95c34eff31594cf05152a
qudehu <qudehu@163.com>
2023-11-07 21:24:46 +0800

    sentinel:添加注释

--- order-openfeign-sentinel/src/main/java/com/dehu/order/feign/StockFeignService.java
+++ order-openfeign-sentinel/src/main/java/com/dehu/order/feign/StockFeignService.java
@@ -4,6 +4,7 @@ import com.dehu.order.config.FeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// 需要指定fallback为指定的实现类
 @FeignClient(name = "stock-nacos", path = "/stock",fallback = StockFeignServiceFallback.class)
 public interface StockFeignService {
 
--- order-openfeign-sentinel/src/main/java/com/dehu/order/feign/StockFeignServiceFallback.java
+++ order-openfeign-sentinel/src/main/java/com/dehu/order/feign/StockFeignServiceFallback.java
@@ -2,6 +2,7 @@ package com.dehu.order.feign;
 
 import org.springframework.stereotype.Component;
 
+//实现接口，并注册为bean
 @Component
 public class StockFeignServiceFallback implements StockFeignService{
     @Override
