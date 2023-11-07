259186628e201e333bbfe1308f5a8aae2def0325
qudehu <qudehu@163.com>
2023-11-07 21:54:25 +0800

    sentinel:热点数据处理

--- order-sentinel/src/main/java/com/dehu/order/controller/OrderController.java
+++ order-sentinel/src/main/java/com/dehu/order/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.dehu.order.controller;
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.dehu.order.service.IOrderService;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -128,6 +129,25 @@ public class OrderController {
     public String err() {
         int a = 1 / 0;
         return "hello";
+    }
+
+    /**
+     * 热点规则，必须使用@SentinelResource
+     * @param id
+     * @return
+     * @throws InterruptedException
+     */
+    @RequestMapping("/get/{id}")
+    @SentinelResource(value = "getById",blockHandler = "HotBlockHandler")
+    public String getById(@PathVariable("id") Integer id) throws InterruptedException {
+
+        System.out.println("正常访问");
+        return "正常访问";
+    }
+
+    public String HotBlockHandler(@PathVariable("id") Integer id,BlockException e) throws InterruptedException {
+
+        return "热点异常处理";
     }
 
 }
