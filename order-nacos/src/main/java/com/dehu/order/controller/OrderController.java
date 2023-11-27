0490427c14791b2d376cd43549a1bc5357939892
qudehu <qudehu@163.com>
2023-11-27 21:47:21 +0800

    gateway:添加依赖、添加配置后，已经整合好了sentinel，开始测试

--- order-nacos/src/main/java/com/dehu/order/controller/OrderController.java
+++ order-nacos/src/main/java/com/dehu/order/controller/OrderController.java
@@ -1,11 +1,13 @@
 package com.dehu.order.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Classname OrderController
@@ -39,6 +41,46 @@ public class OrderController {
     public String header(@RequestHeader("X-Request-color") String color){
         return color;
     }
+
+
+
+    @RequestMapping("/flowThread")
+    //@SentinelResource(value = "flowThread",blockHandler = "flowBlockHandler")
+    public String flowThread() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("正常访问");
+        return "正常访问";
+    }
+
+
+    // 关联流控  访问/add 触发/get
+    @RequestMapping("/get")
+    public String get() throws InterruptedException {
+        return "查询订单";
+    }
+
+
+
+    @RequestMapping("/err")
+    public String err()  {
+        int a=1/0;
+        return "hello";
+    }
+
+
+    /**
+     * 热点规则，必须使用@SentinelResource
+     * @param id
+     * @return
+     * @throws InterruptedException
+     */
+    @RequestMapping("/get/{id}")
+    public String getById(@PathVariable("id") Integer id) throws InterruptedException {
+
+        System.out.println("正常访问");
+        return "正常访问";
+    }
+
 
 
 }
