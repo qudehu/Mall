686effaa8d3780c667faffd1ff0794d0aa6b2d85
qudehu <qudehu@163.com>
2023-12-12 20:47:24 +0800

    skywalking:告警自动回调本地函数进行自定义逻辑处理

---
+++ gateway/src/main/java/com/dehu/gateway/dto/SwAlarmDTO.java
@@ -0,0 +1,40 @@
+package com.dehu.gateway.dto;
+
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+public class SwAlarmDTO {
+
+    /*
+    * "scopeId": 1,
+        "scope": "SERVICE",
+        "name": "serviceB",
+        "id0": "23",
+        "id1": "",
+        "ruleName": "service_resp_time_rule",
+        "alarmMessage": "alarmMessage yyy",
+        "startTime": 1560524171000,
+        "tags": [{
+            "key": "level",
+            "value": "CRITICAL"*/
+    private int scopeId;
+    private String scope;
+    private String name;
+    private String id0;
+    private String id1;
+    private String ruleName;
+    private String alarmMessage;
+    private List<Tag> tags;
+    private long startTime;
+    private transient int period;
+    private transient boolean onlyAsCondition;
+
+    @Data
+    public static class Tag{
+        private String key;
+        private String value;
+    }
+}
